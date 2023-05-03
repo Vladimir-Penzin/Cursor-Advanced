c9a633c49c5432681390a9a7ea511de7e1717347
Volodymyr Penzin <penzinvg@gmail.com>
2023-05-03 23:16:04 +0300

    added HW-16

---
+++ homework_16/src/components/user.js
@@ -0,0 +1,9 @@
+const ANAKIN_IMAGE = "https://i.pinimg.com/originals/b8/98/af/b898af148fded90b232b7d216502931c.png"
+
+const user = {
+    name: "Anakin Skywalker",
+    photo: ANAKIN_IMAGE,
+    nickname: "@dart_vader"
+}
+
+export default user
